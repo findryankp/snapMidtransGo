af26269e99abbc052ca20700b0e58d70cdbe808a
Findryankp <findryankurnia@gmail.com>
2023-04-27 11:37:22 +0700

    callback

--- callback.go
+++ callback.go
@@ -5,14 +5,16 @@ import (
 	"encoding/hex"
 )
 
-type ResponseFromCallback struct {
+type ResponseFromCallbackMidtrans struct {
+	TransactionId     string `json:"transaction_id"`
 	TransactionStatus string `json:"transaction_status"`
 	OrderId           string `json:"order_id"`
 	StatusCode        string `json:"status_code"`
 	SignatureKey      string `json:"signature_key"`
+	GrossAmount       string `json:"gross_amount"`
 }
 
-func ValidateSignatureKey(response ResponseFromCallback, orderId, ServerKey string) bool {
+func ValidateSignatureKey(response ResponseFromCallbackMidtrans, orderId, ServerKey string) bool {
 	str := orderId + response.StatusCode + ServerKey
 	hash := sha512.Sum512([]byte(str))
 	hashStr := hex.EncodeToString(hash[:])
